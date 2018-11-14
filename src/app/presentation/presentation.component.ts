f756a8eb78c9741876b9b6a1286cadc32454f745
karol.kasior <karol.kasior@gmail.com>
2018-11-14 19:06:17 +0100

    Added first chart

--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'pm-presentation',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresentationComponent implements OnInit {
 
+
+  chart = [];
   constructor() { }
+  
 
   ngOnInit() {
+       this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: ['a','b','c','d','e','f','g','h'],
+        datasets: [
+          {
+            label: 'My first dataset',
+            data: [1,3,5,10,56,65,35,543,543,543],
+            backgroundColor: 'red',
+            borderColor: 'red',
+            fill: false,
+          }
+        ], 
+      } 
+      
+    })
   }
 
 }
--- src/app/tests/test.ts
+++ src/app/tests/test.ts
@@ -7,4 +7,5 @@ export interface Itest {
     affectedBy: string; 
     additionalInfo: string;
     testData: string; 
+    testStatus: string;
 }
